d1b6c38b113c8f8af7c31fe3f9b5d299f8d634a4
roescodynex89-sketch <roescody.ne89x@gmail.com>
2026-04-15 21:16:26 +0600

    ddddd

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import AllCard from "./components/AllCard";
 import Banner from "./components/Banner";
 import Summary from "./components/Summary";
 const getFriends = async () => {
-  const res = await fetch("http://localhost:3000/data.json", {
+  const res = await fetch("hero-ass-07.vercel.app/data.json", {
     cache: "no-store",
   });
   return res.json();
